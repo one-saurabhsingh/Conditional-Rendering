0932cb2ab67b7624f59ffd69c6585ac0cc26c4ea
MARCOS <saurabh887799@gmail.com>
2023-03-10 23:15:46 +0530

    Done.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,30 @@
 import React from "react";
+import Login from "./Login";
+
+var isLoggedIn = false;
+
+//function renderConditionally(){
+  // if (isLoggedIn === ture) {
+  //   return  <h1>Hello</h1>
+  // } else {
+  //   return (   <Login />
+  // <form className="form">
+  //   <input type="text" placeholder="Username" />
+  //   <input type="password" placeholder="Password" />
+  //   <button type="submit">Login</button>
+  // </form>
+
+  
+  //   );
+  // }
+//}
 
 function App() {
-  return (
-    <div className="container">
-      <h1>Hello</h1>
-      <form className="form">
-        <input type="text" placeholder="Username" />
-        <input type="password" placeholder="Password" />
-        <button type="submit">Login</button>
-      </form>
-    </div>
-  );
+  return <div className="container">{
+     isLoggedIn ? <h1>Hello</h1>  :  <Login />
+    
+  }</div>
+  
 }
 
 export default App;
---
+++ src/components/Login.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+function Login(){
+    return <form className="form">
+    {/* <input type="text" placeholder="Username" />
+    <input type="password" placeholder="Password" /> */}
+
+    {/* <input 
+    type= "text"
+    placeholder="Username"
+/>
+
+<input 
+    type= "password"
+    placeholder="Password"
+/>
+    
+
+    <button type="submit">Login</button>
+  </form> */}
+}
+
+export default Login;
